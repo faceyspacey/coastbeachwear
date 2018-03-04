e0a93de35523c6c0b82eb3245f836a318f59d3d9
Luc Charbonneau <luc@coastbeachwear.com>
2018-03-04 17:14:09 -0500

    Order summary component now scrolls into view when Item is added.

--- components/OrderSummary/OrderSummary.js
+++ components/OrderSummary/OrderSummary.js
@@ -5,36 +5,56 @@ import { $T, $TInject } from '../../support/translations.js'
 import PurchaseSummary from '../PurchaseSummary/PurchaseSummary.js'
 
 class OrderSummary extends Component {
+	
+	wasPurchaseCount = 0;
+	element = {};
+
 	constructor(props, context) {
 		super();
 	}
 
+	storeDOM(element) {
+		this.element = element;
+	}
+
+	scrollIntoView() {
+
+		this.element.scrollIntoView({ behavior: "smooth" });
+	}
+
 	render() {
 		var order = this.props.order;
+		var purchaseCount = order.purchases.length;
+
+		if (purchaseCount > this.wasPurchaseCount) this.scrollIntoView();
+
+		this.wasPurchaseCount = purchaseCount;
 
 		return (
-			<div className={styles["main"]}>
+			<div className={styles["main"]} ref={ this.storeDOM.bind(this)}>
 				<div className={styles["title"]}>
 					{ $T(16) /* Order Summary */}
 				</div>
-				{ order.purchases.length == 0 &&
-					<div className={styles["empty"]}>
-						{ $T(23) /* Order Empty */ }
-					</div>
-				}
-				{ order.purchases.length > 0 &&
-					<div> {
-						order.purchases.map(function(purchase) {
-							return <PurchaseSummary purchase={ purchase } key={ purchase.variant.id } />
-						})
+				<div className={styles["order-list"]}>
+					{ order.purchases.length == 0 &&
+						<div className={styles["empty"]}>
+							{ $T(23) /* Order Empty */ }
+						</div>
 					}
-						<div className={styles["footer"] }>
-							<div className={styles["subtotal"] }>
-								{ $T(28) + $TInject(27, [this.props.order.calcSubtotal().toFixed(2)]) }
+					{ order.purchases.length > 0 &&
+						<div> {
+							order.purchases.map(function(purchase) {
+								return <PurchaseSummary purchase={ purchase } key={ purchase.variant.id } />
+							})
+						}
+							<div className={styles["footer"] }>
+								<div className={styles["subtotal"] }>
+									{ $T(28) + $TInject(27, [this.props.order.calcSubtotal().toFixed(2)]) }
+								</div>
 							</div>
 						</div>
-					</div>
-				}
+					}
+				</div>
 			</div>
 		)
 	}
