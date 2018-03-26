22a0abb4464a3d2b27748e60ee20ebb8fa353be8
Luc Charbonneau <luc@coastbeachwear.com>
2018-03-26 06:15:08 -0400

    Added hot render component.

---
+++ main/HotRender.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { hot } from 'react-hot-loader'
+import UI from '../components/UI/UI.js'
+import BeachHut from './BeachHut.js'
+
+const HotRender = () => (
+  <div>
+    <UI 
+		locale={ BeachHut.locale }
+		order={ BeachHut.order }
+		products={ BeachHut.products }
+    />
+  </div>
+)
+
+export default hot(module)(HotRender)
